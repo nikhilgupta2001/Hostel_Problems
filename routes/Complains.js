6163559c196ba04d6031aa1ed66cdba68bc47b64
Your Name <you@example.com>
2020-12-16 21:33:27 +0530

    Start

--- routes/Complains.js
+++ routes/Complains.js
@@ -1,18 +1,31 @@
 const mongoose=require('mongoose');
 const express=require('express');
 const router=express.Router();
+const Complain=require('../models/complain');
 
-
-
-
-router.get('/',async(req,res)=>{
-    
-})
 
 router.post('/',async(req,res)=>{
-    const 
-})
+    let complain=new Complain({
+           name:req.body.name,
+           RoomNo:req.body.RoomNo,
+           Complain:req.body.Complain,
+           typeOF:req.body.type,
+           phone:req.body.phone
+    })
+    complain=await complain.save();
+    res.send(complain);
+});
 
-router.get('/',async(req,res)=>{
-    
+
+router.delete('/:id',async(req,res)=>{
+    const complain=await Complain.findById(req.params.id);
+
 })
+module.exports=router;
+
+
+
+
+
+
+
