e64423a1f6aeec917887f10157776880727c3957
SARVESHKHANDELWAL <sarveshk21122001@gmail.com>
2021-01-03 18:39:36 +0530

    make it better

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/complains',complains);
 app.use('/loged',loged);
 //mongoose.connect returns a promise
 
-const connection_url = 'mongodb://localhost/HostelManagement';
+const connection_url = 'mongodb+srv://admin-sarvesh:Sarvesh@21@cluster0-ug5sl.mongodb.net/developerDB';
 
 mongoose.connect(connection_url, {useNewUrlParser:true})
     .then(()=>console.log('Connected to MongoDB...'))
